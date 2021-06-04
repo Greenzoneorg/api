11f5e0b8fb7d4edd28c8dbd0758e1d24a3e4a8e1
Atharv Attri <atharv260107@gmail.com>
2021-06-03 17:48:06 -0700

    update poetry commands

--- api/main.py
+++ api/main.py
@@ -11,6 +11,5 @@ async def root():
     return {"message": "Hello World"}
 
 def start():
-    print(__env.ENVS)
     """Launched with `poetry run start` at root level"""
     uvicorn.run("api.main:app", host=__env.ENVS["HOST"], port=8000, reload=True)
