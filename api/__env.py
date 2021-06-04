171382f099e9e7a9eca8a0c8268482863d149853
Atharv Attri <atharv260107@gmail.com>
2021-06-03 17:42:27 -0700

    set up fastapi

---
+++ api/__env.py
@@ -0,0 +1,8 @@
+from os import environ
+from dotenv import dotenv_values
+
+ENVS = {
+    **dotenv_values(".env.dev"),  # load shared development variables
+    **dotenv_values(".env"),  # load sensitive variables
+    **environ,  # override loaded values with environment variables
+    }
---
+++ api/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+from . import __env
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+def start():
+    print(__env.ENVS)
+    """Launched with `poetry run start` at root level"""
+    uvicorn.run("api.main:app", host=__env.ENVS["HOST"], port=8000, reload=True)
