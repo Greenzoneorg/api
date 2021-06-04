689da8aba08471c6b599dbd6f12584bb3902fb19
Atharv Attri <atharv260107@gmail.com>
2021-06-03 18:02:35 -0700

    set up logging

--- api/main.py
+++ api/main.py
@@ -1,7 +1,20 @@
+import logging
+
 import uvicorn
 from fastapi import FastAPI
 
 from . import __env
+
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
+log = logging.getLogger("rich")
+log.info("Hello, World!")
+
 
 app = FastAPI()
 
