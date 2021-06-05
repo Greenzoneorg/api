023427d85fa4a4d568ed6dd2f7127acd1288548b
Atharv Attri <atharv260107@gmail.com>
2021-06-04 21:08:07 -0700

    retry env

--- api/product_api.py
+++ api/product_api.py
@@ -5,6 +5,11 @@ from typing import Union
 from __env import ENVS
 
 from fastapi import HTTPException
+from dotenv import load_dotenv
+from pathlib import Path
+
+dotenv_path = Path('./env.dev')
+load_dotenv(dotenv_path=dotenv_path)
 
 #connect to db
 deta = Deta(ENVS["DETA_KEY"])
